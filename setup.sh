fd9cda644e72680f61385153f6dd358365c2e0c9
Tom Trahan <tom@shippable.com>
2016-02-29 14:27:46 -0800

    update awscli install, remove grunt

--- setup.sh
+++ setup.sh
@@ -8,3 +8,8 @@ mv /root/micro-image/node_modules .
 
 # install node modules. the above mv will make this zip through as nothing new is installed
 npm install
+
+# install awscli
+apt-get remove python-pip
+easy_install pip
+SUDO=$(which sudo) && $SUDO pip install awscli
